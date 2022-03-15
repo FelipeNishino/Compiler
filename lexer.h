b10453d70aefd9afeb130d037491364277a8aa16
FelipeNishino <felipenishino@gmail.com>
2022-03-15 01:12:41 -0300

    Reading input from stdin

--- lexer.h
+++ lexer.h
@@ -4,6 +4,13 @@
 #include <stdbool.h>
 #include "lexer.c"
 
+typedef enum TokenType {
+    identifier,
+    literal,
+    operat0r,
+    grouper
+} TokenType;
+
 typedef struct Transition {
     int source;
     int target;
@@ -14,14 +21,127 @@ typedef struct State {
     Transition* transitions;
 } State;
 
+typedef struct token {
+    char* token;
+    TokenType type;
+} Token;
+
 typedef struct afd {
     State *states;
 } afd;
 
+//insert link at the first location
+void insertFirst(Register data) {
+    //create a link
+    RegisterNode *link = (RegisterNode *) malloc(sizeof(RegisterNode ));
+
+//    link->key = key;
+    link->reg = data;
+
+    //point it to old first node
+    link->next = head;
+
+    //point first to new first node
+    head = link;
+}
+
+//delete first item
+RegisterNode *deleteFirst() {
+
+    //save reference to first link
+    RegisterNode *tempLink = head;
+
+    //mark next to first link as first
+    head = head->next;
+
+    //return the deleted link
+    return tempLink;
+}
+
+//is list empty
+int isEmpty() {
+    return head == NULL;
+}
+
+int length() {
+    int length = 0;
+    RegisterNode *current;
+
+    for(current = head; current != NULL; current = current->next) {
+        length++;
+    }
+
+    return length;
+}
+
+//find a link with given key
+RegisterNode *find(char *key) {
+
+    //start from the first link
+    RegisterNode *current = head;
+
+    //if list is empty
+    if(head == NULL) {
+        return NULL;
+    }
+
+    //navigate through list
+    while(strcmp(current->reg.name, key) != 0) {
+        //if it is last node
+        if(current->next == NULL) {
+            return NULL;
+        } else {
+            //go to next link
+            current = current->next;
+        }
+    }
+
+    //if data found, return the current Link
+    return current;
+}
+
+//delete a link with given key
+RegisterNode *delete(char key) {
+
+//start from the first link
+RegisterNode *current = head;
+RegisterNode *previous = NULL;
+
+//if list is empty
+if(head == NULL) {
+return NULL;
+}
+
+//navigate through list
+while(strcmp(current->reg.name, key) != 0) {
+
+//if it is last node
+if(current->next == NULL) {
+return NULL;
+} else {
+//store reference to current link
+previous = current;
+//move to next link
+current = current->next;
+}
+}
+
+//found a match, update the link
+if(current == head) {
+//change first to point to next link
+head = head->next;
+} else {
+//bypass the current link
+previous->next = current->next;
+}
+
+return current;
+}
+
 //Transition DEFAULT_TRANSITIONS
 
-char[] ALPHABET = "0123456789let+-*/();";
-0,1,2,3,4,5,6,7,8,9,l,e,t,+,-,*,/,(,),;
-void hello();
+char ALPHABET[] = "0123456789abcdefghijklmnopqrstuwvxyzABCDEFGHIJKLMNOPQRSTUWVXYZ+-*/();";
+
+Token* tokenize(char* input);
 
 #endif //COMPILER_LEXER_H
--- main.c
+++ main.c
@@ -4,9 +4,81 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lexer.h"
 
-int main() {
-    hello();
+#define BUFFER_SIZE 256
+#define STDIN_BUFFER_SIZE 1024
+
+char *getInputFromFile(const char *filename){ 
+    FILE *fp;
+    char basename[20] = "./";
+    char lineBuffer[256];
+    int currentBufferSize = 0;
+    int currentBytes
+    char *input = calloc(256, sizeof(char));
+
+    // Abre o arquivo com o caminho ".\filename"
+    strcat(basename, filename);
+    fp = fopen(basename, "r");
+    // Checa se o fopen foi bem sucedido
+    if (!fp)  {
+        printf("Couldn't find file named '%s', make sure to make the extension explicit.", filename);
+        fclose(fp);
+        return NULL;
+    }
+    
+    while (fgets(lineBuffer, 256, fp)) {
+        currentBufferSize += 256;
+        if (realloc(input, currentBufferSize * sizeof(char)) == NULL) {
+            printf("Error during memory reallocation.\n");
+            return NULL;
+        }
+
+    }
+
+    return input;
+}
+
+void printArray(char* a, int n) {
+    int i = 0;
+    for (i = 0; i < n; i++)
+        printf(" %d ", a[i]);
+}
+
+char *getInputFromStdin() {
+    int line, charPos, nChars = 0, bufferLimit = 1024;
+    char currentChar;
+    char auxStr[2] = {0};
+    char *buffer = calloc(STDIN_BUFFER_SIZE, sizeof(char));
+
+    line = 0;
+    charPos = 0;
+
+    while (1) {
+        currentChar = getchar();
+        nChars++;
+
+        if (nChars > 1 && currentChar == 10 && buffer[nChars-2] == 10) {
+            break;
+        }
+        if (nChars == bufferLimit) {
+            bufferLimit += 1024;
+            if (realloc(buffer, bufferLimit * sizeof(char)) == NULL) {
+                printf("Error during memory reallocation.\n");
+                return NULL;
+            }
+        }
+        
+        auxStr[0] = currentChar;
+        strcat(buffer, auxStr);
+    }
+    return buffer;
+}
+
+int main(int argc, const char *argv[]) {
+    char *input = getInputFromStdin();
+    // printf("Input recebido: /{/%s/}/", input);
+
     return 0;
 }
