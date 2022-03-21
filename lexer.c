f11a10949c26558122c33141986aa6bbaf3a7393
FelipeNishino <felipenishino@gmail.com>
2022-03-21 17:47:57 -0300

    Creating tokenize function in lexer (WIP)

--- lexer.c
+++ lexer.c
@@ -2,7 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lexer.h"
+#include "dfa.h"
 
-TokenNode* tokenize(char* input) {
-    
+TokenNode* tokenize(char* input, DFA *machine) {
+	int i, j, line = 1, col = 1, inputSize = strlen(input) + 1;
+	char currentChar = 0, auxStr[2] = "0";
+	Transition currentTransition;
+	TokenNode *tokens = NULL;
+	State currentState;
+
+	for (i = 0; i < inputSize; i++) {
+		currentChar = input[i];
+
+		if (currentChar == 10) {
+			line++;
+			col = 1;
+			continue;
+		}
+
+		for (j = 0; j < N_TRANSITIONS; j++) {
+			currentTransition = DEFAULT_TRANSITIONS[j];
+			if (currentTransition.source == currentState.id && strchr(currentTransition.read, currentChar)) {
+				currentState = dfa_find(machine, currentTransition.target)->state;
+				
+				if(currentState.isTerminal) {	
+					auxStr[0] = currentChar;
+					tokenList_insertFirst(tokens, (Token) {
+						.token = auxStr,
+						.type = currentState.id,
+						.line = line,
+						.col = col
+						});
+				}
+			}
+		}
+		col++;
+	}
+
+	return tokens;
 }
--- lexer.h
+++ lexer.h
@@ -2,7 +2,8 @@
 #define COMPILER_LEXER_H
 
 #include "token.h"
+#include "dfa.h"
 
-TokenNode* tokenize(char* input);
+TokenNode* tokenize(char* input, DFA *machine);
 
 #endif //COMPILER_LEXER_H
