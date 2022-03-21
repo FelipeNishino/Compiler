2981408564c7f3b780820cfe7fe116560d745c2d
FelipeNishino <felipenishino@gmail.com>
2022-03-21 09:50:09 -0300

    Transferred io code to unique file iomanager

--- main.c
+++ main.c
@@ -1,44 +1,8 @@
-//
-// Created by Nishi on 14/03/2022.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "lexer.h"
-
-#define BUFFER_SIZE 256
-#define STDIN_BUFFER_SIZE 1024
-
-char *getInputFromFile(const char *filename){ 
-    FILE *fp;
-    char basename[20] = "./";
-    char lineBuffer[256];
-    int currentBufferSize = 0;
-    int currentBytes
-    char *input = calloc(256, sizeof(char));
-
-    // Abre o arquivo com o caminho ".\filename"
-    strcat(basename, filename);
-    fp = fopen(basename, "r");
-    // Checa se o fopen foi bem sucedido
-    if (!fp)  {
-        printf("Couldn't find file named '%s', make sure to make the extension explicit.", filename);
-        fclose(fp);
-        return NULL;
-    }
-    
-    while (fgets(lineBuffer, 256, fp)) {
-        currentBufferSize += 256;
-        if (realloc(input, currentBufferSize * sizeof(char)) == NULL) {
-            printf("Error during memory reallocation.\n");
-            return NULL;
-        }
-
-    }
-
-    return input;
-}
+// #include "lexer.h"
+#include "iomanager.h"
 
 void printArray(char* a, int n) {
     int i = 0;
@@ -46,39 +10,17 @@ void printArray(char* a, int n) {
         printf(" %d ", a[i]);
 }
 
-char *getInputFromStdin() {
-    int line, charPos, nChars = 0, bufferLimit = 1024;
-    char currentChar;
-    char auxStr[2] = {0};
-    char *buffer = calloc(STDIN_BUFFER_SIZE, sizeof(char));
-
-    line = 0;
-    charPos = 0;
-
-    while (1) {
-        currentChar = getchar();
-        nChars++;
-
-        if (nChars > 1 && currentChar == 10 && buffer[nChars-2] == 10) {
-            break;
-        }
-        if (nChars == bufferLimit) {
-            bufferLimit += 1024;
-            if (realloc(buffer, bufferLimit * sizeof(char)) == NULL) {
-                printf("Error during memory reallocation.\n");
-                return NULL;
-            }
-        }
-        
-        auxStr[0] = currentChar;
-        strcat(buffer, auxStr);
-    }
-    return buffer;
-}
-
 int main(int argc, const char *argv[]) {
-    char *input = getInputFromStdin();
-    // printf("Input recebido: /{/%s/}/", input);
+    char *input;
+    if (argc > 1) {
+        input = getInputFromFile(argv[1]);
+    }
+    else
+        input = getInputFromStdin();   
+
+    // char *input = getInputFromStdin();
+    
+    printf("Input recebido: /{/%s/}/", input);
 
     return 0;
 }
