6f5aabdc9962e27672a6f2305d37886bc62cc76c
FelipeNishino <felipenishino@gmail.com>
2022-05-02 17:49:17 -0300

    Added function declarations for grammar

--- src/include/parser.h
+++ src/include/parser.h
@@ -9,5 +9,46 @@ typedef struct parser {
 
 Parser* init_parser(char* src);
 int parser_parse(Parser* parser);
+int parser_program(Parser* parser);
+int parser_function(Parser* parser);
+int parser_signature(Parser* parser);
+int parser_argument(Parser* parser);
+int parser_type_annotation(Parser* parser);
+int parser_block(Parser* parser);
+int parser_control_sequence(Parser* parser);
+int parser_std_while(Parser* parser);
+int parser_do_while(Parser* parser);
+int parser_for(Parser* parser);
+int parser_if(Parser* parser);
+int parser_else(Parser* parser);
+int parser_condition(Parser* parser);
+int parser_return(Parser* parser);
+int parser_literal(Parser* parser);
+int parser_statement(Parser* parser);
+int parser_declaration(Parser* parser);
+int parser_declaration_directive(Parser* parser);
+int parser_assignment(Parser* parser);
+int parser_assignment_expression(Parser* parser);
+int parser_simple_expression(Parser* parser);
+int parser_relational_expression(Parser* parser);
+int parser_arithmetic_expression(Parser* parser);
+int parser_grouped_arithmetic_expression(Parser* parser);
+int parser_operand(Parser* parser);
+
+int parser_binary_arithmetic_operator(Parser* parser);
+int parser_unary_arithmetic_operator(Parser* parser);
+int parser_unary_logical_operator(Parser* parser);
+
+int parser_string(Parser* parser);
+int parser_number(Parser* parser);
+int parser_integer(Parser* parser);
+int parser_float(Parser* parser);
+int parser_higher_precedence_binary_arithmetic_operator(Parser* parser);
+int parser_lesser_precedence_binary_arithmetic_operator(Parser* parser);
+int parser_binary_logical_operator(Parser* parser);
+int parser_higher_precedence_binary_logical_operator(Parser* parser);
+int parser_lesser_precedence_binary_logical_operator(Parser* parser);
+int parser_identifier(Parser* parser);
+int parser_type(Parser* parser);
 
 #endif //COMPILER_PARSER_H
--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,6 @@
 #include "include/parser.h"
 #include "include/lexer.h"
+#include "include/variable.h"
 #include <stdlib.h>
 
 Parser* init_parser(char* src) {
@@ -10,14 +11,57 @@ Parser* init_parser(char* src) {
 }
 
 int parser_parse(Parser* parser) {
-	Token* token = 0;
+	// Token* token = 0;
 
-	do {
-		token = lexer_read_token(parser->lexer);
-	}
-	while (token->type != token_EOF);
+	// do {
+	// 	token = lexer_read_token(parser->lexer);
+	// }
+	// while (token->type != token_EOF);
 	
-	printf("%s\n", token_to_str(token));
+	parser_program(parser);
 
-	return 1;
+	// printf("%s\n", token_to_str(token));
+	// printf("printou todos os simbolos");
+	return 0;
 }
+
+int parser_program(Parser* parser) {
+	return 0;
+}
+
+/*
+int cfr_generate_frequency_chart(struct hashmap *map, char* src) {
+    int i = 0;
+    int *cf_array = calloc(95, sizeof(int));
+    // total 95
+    // comeco 32
+    while(src[i]) {
+        if (src[i] == 10) continue;
+        // printf("Vai tentar incrementar a freq em %d para o char %d\n", src[i]-32, src[i]);
+        cf_array[src[i]-32]++;
+        i++;
+    }
+
+    for (i = 0; i < 95; ++i)
+    {
+        printf("char (%d,%c), f %d\n", i+32, i+32, cf_array[i]);
+    }
+
+    
+
+    // hashmap_set(map, &(struct user){ .name="Dale", .age=44 });
+    //     // user = hashmap_get(map, &(struct user){ .name="Jane" });
+
+    // printf("\n-- iterate over all users (hashmap_scan) --\n");
+    // hashmap_scan(map, cfr_iter, NULL);
+
+    // printf("\n-- iterate over all users (hashmap_iter) --\n");
+    // size_t iter = 0;
+    // void *item;
+    // while (hashmap_iter(map, &iter, &item)) {
+    //     const struct user *user = item;
+    //     printf("%s (age=%d)\n", user->name, user->age);
+    // }
+    // hashmap_free(map);
+    return 0;
+}*/
