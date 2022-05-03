f7bb38353713b89d1f0647065c4ffeae5f3c7728
FelipeNishino <49385633+FelipeNishino@users.noreply.github.com>
2022-05-03 17:25:13 -0300

    Added scope to parser structure

--- src/include/parser.h
+++ src/include/parser.h
@@ -2,53 +2,57 @@
 #define COMPILER_PARSER_H
 
 #include "lexer.h"
+#include "token.h"
+#include "variable.h"
 
 typedef struct parser {
 	Lexer* lexer;
+	Scope* globalScope;
 } Parser;
 
 Parser* init_parser(char* src);
-int parser_parse(Parser* parser);
-int parser_program(Parser* parser);
-int parser_function(Parser* parser);
-int parser_signature(Parser* parser);
-int parser_argument(Parser* parser);
-int parser_type_annotation(Parser* parser);
-int parser_block(Parser* parser);
-int parser_control_sequence(Parser* parser);
-int parser_std_while(Parser* parser);
-int parser_do_while(Parser* parser);
-int parser_for(Parser* parser);
-int parser_if(Parser* parser);
-int parser_else(Parser* parser);
-int parser_condition(Parser* parser);
-int parser_return(Parser* parser);
-int parser_literal(Parser* parser);
-int parser_statement(Parser* parser);
-int parser_declaration(Parser* parser);
-int parser_declaration_directive(Parser* parser);
-int parser_assignment(Parser* parser);
-int parser_assignment_expression(Parser* parser);
-int parser_simple_expression(Parser* parser);
-int parser_relational_expression(Parser* parser);
-int parser_arithmetic_expression(Parser* parser);
-int parser_grouped_arithmetic_expression(Parser* parser);
-int parser_operand(Parser* parser);
+int parser_compare_token_type(TokenType t1, TokenType t2);
+void parser_parse(Parser* parser);
+void parser_program(Parser* parser);
+void parser_function(Parser* parser);
+void parser_signature(Parser* parser);
+void parser_argument(Parser* parser);
+void parser_type_annotation(Parser* parser);
+void parser_block(Parser* parser);
+void parser_control_sequence(Parser* parser);
+void parser_std_while(Parser* parser);
+void parser_do_while(Parser* parser);
+void parser_for(Parser* parser);
+void parser_if(Parser* parser);
+void parser_else(Parser* parser);
+void parser_condition(Parser* parser);
+void parser_return(Parser* parser);
+void parser_literal(Parser* parser);
+void parser_statement(Parser* parser);
+void parser_declaration(Parser* parser);
+void parser_declaration_directive(Parser* parser);
+void parser_assignment(Parser* parser);
+void parser_assignment_expression(Parser* parser);
+void parser_simple_expression(Parser* parser);
+void parser_relational_expression(Parser* parser);
+void parser_arithmetic_expression(Parser* parser);
+void parser_grouped_arithmetic_expression(Parser* parser);
+void parser_operand(Parser* parser);
 
-int parser_binary_arithmetic_operator(Parser* parser);
-int parser_unary_arithmetic_operator(Parser* parser);
-int parser_unary_logical_operator(Parser* parser);
+void parser_binary_arithmetic_operator(Parser* parser);
+void parser_unary_arithmetic_operator(Parser* parser);
+void parser_unary_logical_operator(Parser* parser);
 
-int parser_string(Parser* parser);
-int parser_number(Parser* parser);
-int parser_integer(Parser* parser);
-int parser_float(Parser* parser);
-int parser_higher_precedence_binary_arithmetic_operator(Parser* parser);
-int parser_lesser_precedence_binary_arithmetic_operator(Parser* parser);
-int parser_binary_logical_operator(Parser* parser);
-int parser_higher_precedence_binary_logical_operator(Parser* parser);
-int parser_lesser_precedence_binary_logical_operator(Parser* parser);
-int parser_identifier(Parser* parser);
-int parser_type(Parser* parser);
+void parser_string(Parser* parser);
+void parser_number(Parser* parser);
+void parser_integer(Parser* parser);
+void parser_float(Parser* parser);
+void parser_higher_precedence_binary_arithmetic_operator(Parser* parser);
+void parser_lesser_precedence_binary_arithmetic_operator(Parser* parser);
+void parser_binary_logical_operator(Parser* parser);
+void parser_higher_precedence_binary_logical_operator(Parser* parser);
+void parser_lesser_precedence_binary_logical_operator(Parser* parser);
+void parser_identifier(Parser* parser);
+void parser_type(Parser* parser);
 
 #endif //COMPILER_PARSER_H
