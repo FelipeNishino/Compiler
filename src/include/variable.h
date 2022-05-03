f0065acc0a5e2cd603be6e8c7dbc3029e73e26ea
FelipeNishino <49385633+FelipeNishino@users.noreply.github.com>
2022-05-03 17:26:18 -0300

    Added include check

--- src/include/variable.h
+++ src/include/variable.h
@@ -1,18 +1,22 @@
-#include "hashmap.h"
+#ifndef COMPILER_VARIABLE_H
+#define COMPILER_VARIABLE_H
 
-typedef struct hashmap Scope;
+#include "hashmap.h"
 
 typedef enum Type {
 	Int, Float, Bool, String
-}Type;
+} Type;
 
 typedef struct Variable {
 	char* identifier;
 	Type type;
 	void* value;
+	int isconstant;
 } Variable;
 
 int variable_compare(const void *a, const void *b, void *udata);
 bool variable_iter(const void *item, void *udata);
 uint64_t variable_hash(const void *item, uint64_t seed0, uint64_t seed1);
 void variable_new_scope(Scope *s);
+
+#endif COMPILER_VARIABLE_H
