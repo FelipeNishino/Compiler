1cd8a3091e0dda81919970b96c9e218d731c79ed
FelipeNishino <felipenishino@gmail.com>
2022-05-10 04:48:36 -0300

    Added variable initializers + new isStatic property

--- src/include/variable.h
+++ src/include/variable.h
@@ -11,9 +11,12 @@ typedef struct Variable {
 	char* identifier;
 	Type type;
 	void* value;
-	int isconstant;
+	int isConstant;
+	int isStatic;
 } Variable;
 
+Variable* variable_init();
+Variable* variable_init_with_id(const char* id);
 int variable_compare(const void *a, const void *b, void *udata);
 bool variable_iter(const void *item, void *udata);
 uint64_t variable_hash(const void *item, uint64_t seed0, uint64_t seed1);
