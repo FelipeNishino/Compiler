fe794ad59b39a7999d4d003712c30e8adce68ce7
FelipeNishino <felipenishino@gmail.com>
2022-05-23 17:49:57 -0300

    Added Null type, set variable identifier as const, added type assertion function

--- src/include/variable.h
+++ src/include/variable.h
@@ -4,11 +4,11 @@
 #include "hashmap.h"
 
 typedef enum Type {
-	Int, Float, Bool, String
+	Int, Float, Bool, String, Null
 } Type;
 
 typedef struct Variable {
-	char* identifier;
+	const char* identifier;
 	Type type;
 	void* value;
 	int isConstant;
@@ -20,6 +20,7 @@ Variable* variable_init_with_id(const char* id);
 int variable_compare(const void *a, const void *b, void *udata);
 bool variable_iter(const void *item, void *udata);
 uint64_t variable_hash(const void *item, uint64_t seed0, uint64_t seed1);
-void variable_new_scope(Scope *s);
+int variable_assert_type(Type t1, Type t2);
+// void variable_new_scope(Scope *s);
 
-#endif COMPILER_VARIABLE_H
+#endif //COMPILER_VARIABLE_H
