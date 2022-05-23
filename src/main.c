cc73dbce57d2fb2544fcb622d2005e393c773878
FelipeNishino <felipenishino@gmail.com>
2022-05-23 17:50:05 -0300

    Testing hashmap.c usage

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include "include/iomanager.h"
 #include "include/nl.h"
 
+#include "include/variable.h"
+#include "include/scope.h"
+
 void printArray(char* a, int n) {
     int i = 0;
     for (i = 0; i < n; i++)
@@ -9,14 +12,30 @@ void printArray(char* a, int n) {
 }
 
 int main(int argc, const char *argv[]) {
+    if (0) {
+
+
     char *input;
-    // TokenNode *tokens;
+    
     if (argc > 1)
         nl_compile_file(argv[1]);
     else {
         input = getInputFromStdin();
         nl_compile(input);
     }
-    
+    }
+
+    Variable* var1 = variable_init_with_id("var1");
+
+    Scope* scope = scope_init();
+    scope_set_variable(scope, *var1);
+    Variable* var2 = scope_get_variable_by_id(scope, "var1");
+    int val = 10;
+    var2->value = &val;
+    printf("valor var1: %d\n", *(int*)var2->value);
+    scope_set_variable(scope, *var2);
+    var1 = scope_get_variable_by_id(scope, "var1");
+    printf("valor var1: %d\n", *(int*)var1->value);
+
     return 0;
 }
