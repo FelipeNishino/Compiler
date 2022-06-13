f7f0cc909ef99b3f1a00bae92b8a883aa1d347e4
FelipeNishino <felipenishino@gmail.com>
2022-06-13 01:30:00 -0300

    Added support for the -l (lexer only) flag

--- src/main.c
+++ src/main.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 #include "include/iomanager.h"
 #include "include/nl.h"
-
 #include "include/variable.h"
 #include "include/scope.h"
 
-void printArray(char* a, int n) {
-    int i = 0;
-    for (i = 0; i < n; i++)
-        printf(" %d ", a[i]);
-}
+int main(int argc, const char *argv[]) {
+    int lexer_only = 0;
 
-int main(int argc, const char *argv[]) {    
-    char *input;
-
-    if (argc > 1)
-        nl_compile_file(argv[1]);
-    else {
-        input = getInputFromStdin();
-        nl_compile(input);
+    // Extrai as opcoes nos argumentos para o algoritmo
+    if (argc < 2) fprintf(stderr, "Please provide adequate arguments.");
+    for(int i = 2; i < argc; ++i) {
+        // if (strcmp(argv[i], "-v") == 0) set_verbose(1);
+        if (strcmp(argv[i], "-l") == 0) lexer_only = 1;
+        else {
+            fprintf(stderr, "[nl:main.c]: Unrecognized argument '%s'.\n", argv[i]);
+            return 1;
+        }
     }
+    
+    if (lexer_only)
+        nl_tokenize_file(argv[1]);
+    else 
+        nl_compile_file(argv[1]);
     
     return 0;
 }
