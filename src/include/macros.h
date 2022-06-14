69646cfd1f4d13a16ad6bd4a6f7deace191d474c
FelipeNishino <felipenishino@gmail.com>
2022-06-14 15:58:43 -0300

    Added ABS macro

--- src/include/macros.h
+++ src/include/macros.h
@@ -3,6 +3,7 @@
 #define LEN(x) (sizeof(x) / sizeof((x)[0]))
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 #define MAX(X, Y) (((X) < (Y)) ? (Y) : (X))
+#define ABS(X) (((X) < 0) ? -(X) : (X))
 
 #define GENERATE_ENUM(ENUM) ENUM,
 #define GENERATE_STRING(STRING) #STRING,
